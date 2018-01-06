f106abe85b2aa788f8be857f29de7e6e848e23f3
taka66 <taka66.iphone@gmail.com>
2018-01-06 14:54:10 +0900

    1st commit.

---
+++ spec/beepro-dancer-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BeeproDancerAtomView from '../lib/beepro-dancer-atom-view';
+
+describe('BeeproDancerAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
